210af6f7a2e519d56810301c587272d8e7b21cf8
SatyarthSuman <78637434+SatyarthSuman@users.noreply.github.com>
2021-02-06 16:34:36 +0530

    My Code For Assignment

---
+++ code.py
@@ -0,0 +1,27 @@
+nameOfGoodies = input('Enter the goodies :').split()
+priceOfGoddies = input('Enter the price of goodies :').split()
+res = {} 
+lenOfGoodies = len(nameOfGoodies)
+for key in nameOfGoodies: 
+    for value in priceOfGoddies: 
+        res[key] = value 
+        priceOfGoddies.remove(value) 
+        break
+
+
+#print(res)
+res1 = dict(sorted(res.items(), key=lambda item: item[1]))
+#print(res1)
+
+
+numberOfEmployee = int(input("Enter the number of emplyee :"))
+#print('...............................................................')
+if (lenOfGoodies >= numberOfEmployee):
+    arr = res1.values()
+    #print(arr)
+    list_arr = list(arr)
+    #print('List of arr', list_arr)
+    #print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    print(int(list_arr[numberOfEmployee -1]) - int(list_arr[0]))
+else:
+    print('Number of goodies should be greater or equal to Number of Employee')
